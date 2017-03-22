77c8fb19baa1cd93d93a9b91a52c3653e0bed0a5
Bastian Kalthoff <bastian.kalthoff@hotmail.com>
2017-03-22 23:36:25 +0100

    Ping function. Because why not?

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import discord
 import re
+import time
 import youtube_dl
 token = 'Mjk0MTM1Mjg0MjczMjUwMzA1.C7QukQ.gSdGLmWXhKsvjHf9CYgYfCQQfPw'
 client = discord.Client()
@@ -14,11 +15,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
-    if message.author == client.user.name:
-        return
+    #if message.author == client.user.name:
+     #   return
     usr = message.author
     print(usr, message.content)  # chatlogger
-    print()
+
     if message.content.startswith('--join'):
         urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.content)
 
@@ -29,7 +30,23 @@ async def on_message(message: discord.Message):
         voice = await client.join_voice_channel(join_channel)
         player = await voice.create_ytdl_player(urls[0])
         player.start()
-        
+
+    global millisecond
+    global ping
+
+    if message.content.startswith('ping'):
+
+        millisecond = int(round(time.time() * 1000))
+        print(millisecond)
+        await client.send_message(message.channel, 'pong')
+        print (client.user)
+    if message.content.startswith('pong') and message.author == client.user:
+        ping = int(round(time.time() * 1000))
+        print (ping)
+        print (ping - millisecond)
+        await client.send_message(message.channel, ping - millisecond)
+
+
 
     if (message.content.startswith('leave')):
         await discord.VoiceClient.disconnect()
